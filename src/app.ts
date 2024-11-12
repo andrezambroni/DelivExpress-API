1faec3c4929c524234eb81aa2de366ebb4b5722f
andrezambroni <andrezambroni@gmail.com>
2024-11-12 16:11:00 -0300

    feat: criando rota e controller de usuario

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import "express-async-errors"
+
 import { errorHandling } from "./middlewares/error-handling"
+import { routes } from "./routes"
 
 const app = express()
 
+app.use(routes)
 app.use(express.json())
 
 app.use(errorHandling)
